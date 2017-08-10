ec1cb6129f51ccccb07fd94af3ea70a36a679b70
Jeff Bader <redabaj@gmail.com>
2017-08-10 15:21:39 -0700

    Now with promises.

--- index.js
+++ index.js
@@ -1,23 +1,57 @@
-var env = require('./env');
-var couchdb = require('nano');
+let env = require('./env');
+let Promise = require('promise');
 
-var nano = require('nano')('http://' + env.COUCHDB_SERVER + ':' + env.COUCHDB_PORT);
-var sourceDb = nano.use(env.COUCHDB_DB);
+function getDocument(documentId) {
+    let nano = require('nano')('http://' + env.COUCHDB_SERVER + ':' + env.COUCHDB_PORT);
+    let sourceDb = nano.use(env.COUCHDB_DB);
 
-// get a single document from couch DB
-sourceDb.get('00007290-7630-4cff-8b27-27c555276bbb', function (err, body) {
-    console.log('retrieved document!');
-    console.log(JSON.stringify(body));
-});
-
-// get all the document ids
-sourceDb.list(function (err, body) {
-    if (err) {
-        console.error(err);
-    }
-    else {
-        body.rows.forEach(function (doc) {
-            console.log(doc);
+    let promise = new Promise((resolve, reject) => {
+        sourceDb.get(documentId, (err, doc) => {
+            if (err) {
+                reject(err);
+            }
+            else {
+                resolve(doc);
+            }
         });
-    }
-});
+    });
+    return promise;
+}
+
+function getAllDocumentIds() {
+    let nano = require('nano')('http://' + env.COUCHDB_SERVER + ':' + env.COUCHDB_PORT);
+    let sourceDb = nano.use(env.COUCHDB_DB);
+    let promise = new Promise((resolve, reject) => {
+        sourceDb.list((err, body) => {
+            if (err) {
+                reject(err);
+            }
+            else {
+                let docIds = body.rows.map((row) => row.id);
+                resolve(docIds);
+            }
+        });
+    });
+    return promise;
+}
+
+function main() {
+
+    getDocument('00007290-7630-4cff-8b27-27c555276bbb').then((doc) => {
+        console.log('retrieved document!');
+        console.log(JSON.stringify(doc));
+    });
+
+    getAllDocumentIds().then((docIds) => {
+        docIds.forEach((docId) => {
+            console.log(docId);
+            // getDocument(docId).then((doc) => {
+            //     console.log(docId);
+            // });
+        });
+    });
+
+}
+
+main();
+
